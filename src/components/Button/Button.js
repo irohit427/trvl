c1c7627c90b1dab9992a1d151d115e0486dbaba9
Rohit Raj <irohit427@gmail.com>
2020-09-27 15:22:57 +0530

    Craeted Navbar

---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Button.css';
+
+const STYLES = ['btn--primary', 'btn--outline'];
+const SIZES = ['btn--medium', 'btn--large'];
+
+
+export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+  const button_style = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  const button_size = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to='/sign-up' className='btn-mobile'>
+      <button className={`btn ${button_style} ${button_size}`} onClick={onClick} type={type}>
+        {children}
+      </button>
+    </Link>
+  )
+};
