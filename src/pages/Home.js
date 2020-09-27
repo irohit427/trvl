9e57efcccb2d0daf5f546ae9a84b93b823f0e24c
Rohit Raj <irohit427@gmail.com>
2020-09-27 20:19:00 +0530

    Added Hero Card and Card Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import '../App.css';
 import HeroSection from '../components/HeroSection/HeroSection'
 import React from 'react'
+import CardsSection from '../components/CardsSection/CardsSection';
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <CardsSection />
     </div>
   )
 }
